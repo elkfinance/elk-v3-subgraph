5b4b31ec0c208501e1041de51144af9acd1fd1e7
LtSnakePlissken <snake@elklabs.org>
2024-10-29 21:02:21 -0600

    Added Kava graph

---
+++ kava/src/utils/chains.ts
@@ -0,0 +1,99 @@
+import { Address, BigDecimal, BigInt, dataSource } from '@graphprotocol/graph-ts'
+
+import { OPTIMISM_POOL_MAPPINGS } from '../backfill/poolMappings'
+import { StaticTokenDefinition } from './staticTokenDefinition'
+
+export enum ChainId {
+  MAINNET = 2222,
+}
+
+// subgraph does not support string enums, hence these constants
+
+const MAINNET_NETWORK_NAME = 'mainnet'
+
+// Note: All token and pool addresses should be lowercased!
+export class SubgraphConfig {
+  // deployment address
+  // e.g. https://docs.uniswap.org/contracts/v3/reference/deployments/ethereum-deployments
+  factoryAddress: string
+
+  // the address of a pool where one token is a stablecoin and the other is a
+  // token that tracks the price of the native token use this to calculate the
+  // price of the native token, so prefer a pool with highest liquidity
+  stablecoinWrappedNativePoolAddress: string
+
+  // true is stablecoin is token0, false if stablecoin is token1
+  stablecoinIsToken0: boolean
+
+  // the address of a token that tracks the price of the native token, most of
+  // the time, this is a wrapped asset but could also be the native token itself
+  // for some chains
+  wrappedNativeAddress: string
+
+  // the mimimum liquidity in a pool needed for it to be used to help calculate
+  // token prices. for new chains, this should be initialized to ~4000 USD
+  minimumNativeLocked: BigDecimal
+
+  // list of stablecoin addresses
+  stablecoinAddresses: string[]
+
+  // a token must be in a pool with one of these tokens in order to derive a
+  // price (in addition to passing the minimumEthLocked check). This is also
+  // used to determine whether volume is tracked or not.
+  whitelistTokens: string[]
+
+  // token overrides are used to override RPC calls for the symbol, name, and
+  // decimals for tokens. for new chains this is typically empty.
+  tokenOverrides: StaticTokenDefinition[]
+
+  // skip the creation of these pools in handlePoolCreated. for new chains this is typically empty.
+  poolsToSkip: string[]
+
+  // initialize this list of pools and token addresses on factory creation. for new chains this is typically empty.
+  poolMappings: Array<Address[]>
+}
+
+export function getSubgraphConfig(): SubgraphConfig {
+  // Update this value to the corresponding chain you want to deploy
+  const selectedNetwork = dataSource.network()
+
+  // subgraph does not support case switch with strings, hence this if else block
+  if (selectedNetwork == MAINNET_NETWORK_NAME) {
+    return {
+      factoryAddress: '0xc05a5aa56df0dc97d6b9849a06627a079790014f',
+      stablecoinWrappedNativePoolAddress: '0x0375cfbd69327eee285268905777c9e6f970ae20', // WETH-USDC 0.05% pool
+      stablecoinIsToken0: true,
+      wrappedNativeAddress: '0xc86c7c0efbd6a49b35e8714c5f59d99de09a225b', // WETH
+      minimumNativeLocked: BigDecimal.fromString('0'),
+      stablecoinAddresses: [
+        '0x919c1c267bc06a7039e03fcc2ef738525769109c', // USDT
+        '0xeb466342c4d449bc9f53a865d5cb90586f405215', // USDC Axelar
+      ],
+      whitelistTokens: [
+        '0xeeeeeb57642040be42185f49c52f7e9b38f8eeee', // elk
+        '0xe1c110e1b1b4a1ded0caf3e42bfbdbb7b5d7ce1c', // old_elk
+        '0xe1c8f3d529bea8e3fa1fac5b416335a2f998ee1c', // elk_legacy
+        '0xb5c4423a65b953905949548276654c96fcae6992', // WBTC
+        '0x1a35ee4640b0a3b87705b0a4b45d227ba60ca2ad', // Axelar WBTC
+      ],
+      tokenOverrides: [
+        // {
+        //   address: Address.fromString('0x82af49447d8a07e3bd95bd0d56f35241523fbab1'),
+        //   symbol: 'WETH',
+        //   name: 'Wrapped Ethereum',
+        //   decimals: BigInt.fromI32(18),
+        // },
+        // {
+        //   address: Address.fromString('0xff970a61a04b1ca14834a43f5de4533ebddb5cc8'),
+        //   symbol: 'USDC.e',
+        //   name: 'USD Coin from Ethereum',
+        //   decimals: BigInt.fromI32(6),
+        // },
+      ],
+      poolsToSkip: [],
+      poolMappings: [],
+    }
+  }  else {
+    throw new Error('Unsupported Network')
+  }
+}
---
+++ kava/src/utils/pricing.ts
@@ -0,0 +1,148 @@
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+
+import { exponentToBigDecimal, safeDiv } from '../utils/index'
+import { Bundle, Pool, Token } from './../types/schema'
+import { ONE_BD, ZERO_BD, ZERO_BI } from './constants'
+
+export const WETH_ADDRESS = '0xc86c7c0efbd6a49b35e8714c5f59d99de09a225b'
+export const USDC_WETH_03_POOL = '0x0375cfbd69327eee285268905777c9e6f970ae20'
+export const STABLECOIN_IS_TOKEN0 = true
+
+// token where amounts should contribute to tracked volume and liquidity
+// usually tokens that many tokens are paired with s
+export const WHITELIST_TOKENS: string[] = [
+  WETH_ADDRESS, // WETH
+  '0xeeeeeb57642040be42185f49c52f7e9b38f8eeee', // elk
+  '0xe1c110e1b1b4a1ded0caf3e42bfbdbb7b5d7ce1c', // old_elk
+  '0xe1c8f3d529bea8e3fa1fac5b416335a2f998ee1c', // elk_legacy
+  '0xb5c4423a65b953905949548276654c96fcae6992', // WBTC
+  '0x1a35ee4640b0a3b87705b0a4b45d227ba60ca2ad', // Axelar WBTC
+]
+
+export const STABLE_COINS: string[] = [
+  '0x919c1c267bc06a7039e03fcc2ef738525769109c', // USDT
+  '0xeb466342c4d449bc9f53a865d5cb90586f405215', // USDC Axelar
+]
+
+export const MINIMUM_ETH_LOCKED = BigDecimal.fromString('60')
+
+const Q192 = BigInt.fromI32(2).pow(192 as u8)
+export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, token1: Token): BigDecimal[] {
+  const num = sqrtPriceX96.times(sqrtPriceX96).toBigDecimal()
+  const denom = BigDecimal.fromString(Q192.toString())
+  const price1 = num.div(denom).times(exponentToBigDecimal(token0.decimals)).div(exponentToBigDecimal(token1.decimals))
+
+  const price0 = safeDiv(BigDecimal.fromString('1'), price1)
+  return [price0, price1]
+}
+
+export function getNativePriceInUSD(
+  stablecoinWrappedNativePoolAddress: string,
+  stablecoinIsToken0: boolean,
+): BigDecimal {
+  const stablecoinWrappedNativePool = Pool.load(stablecoinWrappedNativePoolAddress)
+  if (stablecoinWrappedNativePool !== null) {
+    return stablecoinIsToken0 ? stablecoinWrappedNativePool.token0Price : stablecoinWrappedNativePool.token1Price
+  } else {
+    return ZERO_BD
+  }
+}
+
+/**
+ * Search through graph to find derived Eth per token.
+ * @todo update to be derived ETH (add stablecoin estimates)
+ **/
+export function findNativePerToken(
+  token: Token,
+  wrappedNativeAddress: string,
+  stablecoinAddresses: string[],
+  minimumNativeLocked: BigDecimal,
+): BigDecimal {
+  if (token.id == wrappedNativeAddress) {
+    return ONE_BD
+  }
+  const whiteList = token.whitelistPools
+  // for now just take USD from pool with greatest TVL
+  // need to update this to actually detect best rate based on liquidity distribution
+  let largestLiquidityETH = ZERO_BD
+  let priceSoFar = ZERO_BD
+  const bundle = Bundle.load('1')!
+
+  // hardcoded fix for incorrect rates
+  // if whitelist includes token - get the safe price
+  if (stablecoinAddresses.includes(token.id)) {
+    priceSoFar = safeDiv(ONE_BD, bundle.ethPriceUSD)
+  } else {
+    for (let i = 0; i < whiteList.length; ++i) {
+      const poolAddress = whiteList[i]
+      const pool = Pool.load(poolAddress)
+
+      if (pool) {
+        if (pool.liquidity.gt(ZERO_BI)) {
+          if (pool.token0 == token.id) {
+            // whitelist token is token1
+            const token1 = Token.load(pool.token1)
+            // get the derived ETH in pool
+            if (token1) {
+              const ethLocked = pool.totalValueLockedToken1.times(token1.derivedETH)
+              if (ethLocked.gt(largestLiquidityETH) && ethLocked.gt(minimumNativeLocked)) {
+                largestLiquidityETH = ethLocked
+                // token1 per our token * Eth per token1
+                priceSoFar = pool.token1Price.times(token1.derivedETH as BigDecimal)
+              }
+            }
+          }
+          if (pool.token1 == token.id) {
+            const token0 = Token.load(pool.token0)
+            // get the derived ETH in pool
+            if (token0) {
+              const ethLocked = pool.totalValueLockedToken0.times(token0.derivedETH)
+              if (ethLocked.gt(largestLiquidityETH) && ethLocked.gt(minimumNativeLocked)) {
+                largestLiquidityETH = ethLocked
+                // token0 per our token * ETH per token0
+                priceSoFar = pool.token0Price.times(token0.derivedETH as BigDecimal)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return priceSoFar // nothing was found return 0
+}
+
+/**
+ * Accepts tokens and amounts, return tracked amount based on token whitelist
+ * If one token on whitelist, return amount in that token converted to USD * 2.
+ * If both are, return sum of two amounts
+ * If neither is, return 0
+ */
+export function getTrackedAmountUSD(
+  tokenAmount0: BigDecimal,
+  token0: Token,
+  tokenAmount1: BigDecimal,
+  token1: Token,
+  whitelistTokens: string[],
+): BigDecimal {
+  const bundle = Bundle.load('1')!
+  const price0USD = token0.derivedETH.times(bundle.ethPriceUSD)
+  const price1USD = token1.derivedETH.times(bundle.ethPriceUSD)
+
+  // both are whitelist tokens, return sum of both amounts
+  if (whitelistTokens.includes(token0.id) && whitelistTokens.includes(token1.id)) {
+    return tokenAmount0.times(price0USD).plus(tokenAmount1.times(price1USD))
+  }
+
+  // take double value of the whitelisted token amount
+  if (whitelistTokens.includes(token0.id) && !whitelistTokens.includes(token1.id)) {
+    return tokenAmount0.times(price0USD).times(BigDecimal.fromString('2'))
+  }
+
+  // take double value of the whitelisted token amount
+  if (!whitelistTokens.includes(token0.id) && whitelistTokens.includes(token1.id)) {
+    return tokenAmount1.times(price1USD).times(BigDecimal.fromString('2'))
+  }
+
+  // neither token is on white list, tracked amount is 0
+  return ZERO_BD
+}
