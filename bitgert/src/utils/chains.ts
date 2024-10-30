a3d9075077dafaf2f3e627ed3ac9b5d5588c7014
LtSnakePlissken <snake@elklabs.org>
2024-10-29 23:08:03 -0600

    Added Bitgert

---
+++ bitgert/src/utils/chains.ts
@@ -0,0 +1,95 @@
+import { Address, BigDecimal, BigInt, dataSource } from '@graphprotocol/graph-ts'
+
+import { OPTIMISM_POOL_MAPPINGS } from '../backfill/poolMappings'
+import { StaticTokenDefinition } from './staticTokenDefinition'
+
+export enum ChainId {
+  MAINNET = 32520,
+}
+
+// subgraph does not support string enums, hence these constants
+
+const MAINNET_NETWORK_NAME = 'mainnet'
+
+// Note: All token and pool addresses should be lowercased!
+export class SubgraphConfig {
+  // deployment address
+  // e.g. https://docs.uniswap.org/contracts/v3/reference/deployments/ethereum-deployments
+  factoryAddress: string
+
+  // the address of a pool where one token is a stablecoin and the other is a
+  // token that tracks the price of the native token use this to calculate the
+  // price of the native token, so prefer a pool with highest liquidity
+  stablecoinWrappedNativePoolAddress: string
+
+  // true is stablecoin is token0, false if stablecoin is token1
+  stablecoinIsToken0: boolean
+
+  // the address of a token that tracks the price of the native token, most of
+  // the time, this is a wrapped asset but could also be the native token itself
+  // for some chains
+  wrappedNativeAddress: string
+
+  // the mimimum liquidity in a pool needed for it to be used to help calculate
+  // token prices. for new chains, this should be initialized to ~4000 USD
+  minimumNativeLocked: BigDecimal
+
+  // list of stablecoin addresses
+  stablecoinAddresses: string[]
+
+  // a token must be in a pool with one of these tokens in order to derive a
+  // price (in addition to passing the minimumEthLocked check). This is also
+  // used to determine whether volume is tracked or not.
+  whitelistTokens: string[]
+
+  // token overrides are used to override RPC calls for the symbol, name, and
+  // decimals for tokens. for new chains this is typically empty.
+  tokenOverrides: StaticTokenDefinition[]
+
+  // skip the creation of these pools in handlePoolCreated. for new chains this is typically empty.
+  poolsToSkip: string[]
+
+  // initialize this list of pools and token addresses on factory creation. for new chains this is typically empty.
+  poolMappings: Array<Address[]>
+}
+
+export function getSubgraphConfig(): SubgraphConfig {
+  // Update this value to the corresponding chain you want to deploy
+  const selectedNetwork = dataSource.network()
+
+  // subgraph does not support case switch with strings, hence this if else block
+  if (selectedNetwork == MAINNET_NETWORK_NAME) {
+    return {
+      factoryAddress: '0xc05a5aa56df0dc97d6b9849a06627a079790014f',
+      stablecoinWrappedNativePoolAddress: '0x299476784e09c3965fc384540a233d3544eca82f', // WBRISE-USDTi 0.3% pool
+      stablecoinIsToken0: false,
+      wrappedNativeAddress: '0x0eb9036cbe0f052386f36170c6b07ef0a0e3f710', // WBRISE
+      minimumNativeLocked: BigDecimal.fromString('0'),
+      stablecoinAddresses: [
+        '0xc7e6d7e08a89209f02af47965337714153c529f0', // USDTi
+      ],
+      whitelistTokens: [
+        '0xeeeeeb57642040be42185f49c52f7e9b38f8eeee', // ELK
+        '0xb999ea90607a826a3e6e6646b404c3c7d11fa39d', // ICE
+      ],
+      tokenOverrides: [
+        // {
+        //   address: Address.fromString('0x82af49447d8a07e3bd95bd0d56f35241523fbab1'),
+        //   symbol: 'WETH',
+        //   name: 'Wrapped Ethereum',
+        //   decimals: BigInt.fromI32(18),
+        // },
+        // {
+        //   address: Address.fromString('0xff970a61a04b1ca14834a43f5de4533ebddb5cc8'),
+        //   symbol: 'USDC.e',
+        //   name: 'USD Coin from Ethereum',
+        //   decimals: BigInt.fromI32(6),
+        // },
+      ],
+      poolsToSkip: [],
+      poolMappings: [],
+    }
+  }  else {
+    throw new Error('Unsupported Network')
+  }
+}
